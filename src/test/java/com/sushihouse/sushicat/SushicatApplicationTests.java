005e4b6bbffbabdd32ee58acf9c48372d2f39b24
zaehuun <51132077+zaehuun@users.noreply.github.com>
2021-05-28 15:09:02 +0900

    Initial commit

---
+++ src/test/java/com/sushihouse/sushicat/SushicatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sushihouse.sushicat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SushicatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
