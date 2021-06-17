898cc299aa1c8412bd45fbb9ab00ad80e3359910
zaehuun <51132077+zaehuun@users.noreply.github.com>
2021-06-17 22:32:31 +0900

    add CORS config

--- back-end/back-end/src/main/java/com/project/backend/config/WebConfig.java
+++ back-end/back-end/src/main/java/com/project/backend/config/WebConfig.java
@@ -1,2 +1,16 @@
-package com.project.backend.config;public class WebConfig {
+package com.project.backend.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration //어노테이션으로 설정 파일임을 알려줌
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry){
+        registry.addMapping("/**") //자원 공유 허락할 origin 지정
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("GET","POST");
+    }
 }
