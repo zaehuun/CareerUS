2170bded860ccc93c85109fcac4f3c52df112719
EasyBAMM <huhjb1020@naver.com>
2021-06-25 14:48:13 +0900

    init frontend

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Route } from "react-router-dom";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+import MainPage from "./pages/MainPage";
+// import PostListPage from "./pages/PostListPage";
+// import WritePage from "./pages/WritePage";
+// import PostPage from "./pages/PostPage";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Route component={MainPage} path="/" exact={true} />
+      <Route component={LoginPage} path="/login" />
+      <Route component={RegisterPage} path="/register" />
+      {/* <Route component={PostListPage} path="/@:username" />
+      <Route component={WritePage} path="/write" />
+      <Route component={PostPage} path="/@:username/:postId" /> */}
+    </>
   );
-}
+};
 
 export default App;
