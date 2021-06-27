9d587bd333f36c7a5c607870755145e5e2eada0f
EasyBAMM <huhjb1020@naver.com>
2021-06-27 17:57:39 +0900

    add footer

--- front-end/src/pages/LoginPage.js
+++ front-end/src/pages/LoginPage.js
@@ -1,13 +1,8 @@
 import React from "react";
-import PageTemplate from "../components/common/PageTemplate/PageTemplate";
 import LoginContainer from "../containers/Login/LoginContainer.";
 
 const LoginPage = () => {
-  return (
-    <PageTemplate>
-      <LoginContainer />
-    </PageTemplate>
-  );
+  return <LoginContainer />;
 };
 
 export default LoginPage;
