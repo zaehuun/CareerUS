de80a814a8fcd681f64a6c7ed5495f276b0d7086
EasyBAMM <huhjb1020@naver.com>
2021-07-02 07:08:03 +0900

    add logout and redirect

--- front-end/src/containers/common/Header/HeaderContainer.js
+++ front-end/src/containers/common/Header/HeaderContainer.js
@@ -1,10 +1,25 @@
-import React from "react";
-import { useSelector } from "react-redux";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { withRouter } from "react-router-dom";
 import Header from "../../../components/common/Header/Header";
+import { logout } from "../../../modules/user";
 
-const HeaderContainer = () => {
-  const { auth } = useSelector(({ auth }) => ({ auth: auth.auth }));
-  return <Header auth={auth} />;
+const HeaderContainer = ({ history }) => {
+  const { user } = useSelector(({ user }) => ({ user: user.user }));
+  const dispatch = useDispatch();
+  const onLogout = () => {
+    dispatch(logout());
+  };
+
+  // user 값이 없다면 로그인 화면으로
+  useEffect(() => {
+    if (!user) {
+      console.log("잘못된 접근입니다.");
+      history.push("/"); // 로그인 페이지로 이동
+    }
+  }, [history, user]);
+
+  return <Header user={user} onLogout={onLogout} />;
 };
 
-export default HeaderContainer;
+export default withRouter(HeaderContainer);
