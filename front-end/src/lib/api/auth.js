26b8c9516365b3791cbcbaa93886cbdb6a8ee71d
EasyBAMM <huhjb1020@naver.com>
2021-07-02 07:15:19 +0900

    fix path

--- front-end/src/lib/api/auth.js
+++ front-end/src/lib/api/auth.js
@@ -2,7 +2,7 @@ import client from "./client";
 
 // 로그인
 export const login = ({ username, password }) =>
-  client.post("/login", { username, password });
+  client.post("/api/auth/login", { username, password });
 
 // 회원가입
 export const register = ({
@@ -13,7 +13,7 @@ export const register = ({
   name,
   comment,
 }) =>
-  client.post("/register", {
+  client.post("/api/auth/register", {
     username,
     password,
     passwordConfirm,
@@ -23,4 +23,7 @@ export const register = ({
   });
 
 // 로그인 상태 확인
-export const check = () => client.get("/check");
+export const check = () => client.get("/api/auth/check");
+
+// 로그아웃
+export const logout = () => client.post("/api/auth/logout");
